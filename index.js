95d1215afa872310b2f5085997c3b1cf40a4cc0e
Misha <misha.shalkow@gmail.com>
2024-10-29 00:18:03 +0300

    remake project

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const download = document.getElementById("price");
 
 download.addEventListener("click", () => {
-  const data = "/price.xlsm";
-  const file = new Blob([data], { type: "application/vnd.ms-excel" });
   const link = document.createElement("a");
-  link.href = URL.createObjectURL(file);
   link.download = "price.xlsm";
   link.click();
 });
